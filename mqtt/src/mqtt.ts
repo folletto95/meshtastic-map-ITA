1026e6a44c4c5c1b394707ebd657bb4551a28f4e
Tilen Komel <me@komelt.dev>
2024-08-24 20:13:17 +0200

    Updated mqtt config

--- mqtt/src/mqtt.ts
+++ mqtt/src/mqtt.ts
@@ -1,19 +1,21 @@
 import mqtt from "mqtt";
 import {
-  MQTT_URL,
-  MQTT_USERNAME,
-  MQTT_PASSWORD,
-  MQTT_TOPIC,
+	MQTT_URL,
+	MQTT_USERNAME,
+	MQTT_PASSWORD,
+	MQTT_TOPIC,
+	MQTT_PROTOCOL,
 } from "./settings.js";
 
 export const mqttClient = mqtt.connect(MQTT_URL, {
-  username: MQTT_USERNAME,
-  password: MQTT_PASSWORD,
+	username: MQTT_USERNAME,
+	password: MQTT_PASSWORD,
+	protocol: MQTT_PROTOCOL,
 });
 
 mqttClient.on("connect", () => {
-  mqttClient.subscribe(MQTT_TOPIC);
-  console.log(`
+	mqttClient.subscribe(MQTT_TOPIC);
+	console.log(`
     .- - - - - - - - - - - - - - - - - - - -
     |   MQTT client listening on topic ${MQTT_TOPIC}
     '- - - - - - - - - - - - - - - - - - - -
@@ -21,5 +23,5 @@ mqttClient.on("connect", () => {
 });
 
 mqttClient.on("error", (error: Error) => {
-  console.error(error);
+	console.error(error);
 });
