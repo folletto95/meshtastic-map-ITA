fad9e50a06e7d3da36d63a49ffd0476728c670d7
liamcottle <liam@liamcottle.com>
2024-09-02 03:26:18 +1200

    implement new device metrics chart

--- api/src/routes/metrics.ts
+++ api/src/routes/metrics.ts
@@ -2,179 +2,189 @@ import { prisma } from "../db.js";
 import express from "../express.js";
 
 express.get("/api/v1/nodes/:nodeId/device-metrics", async (req, res) => {
-  try {
-    const nodeId = Number.parseInt(req.params.nodeId);
-    const count = req.query.count
-      ? Number.parseInt(req.query.count.toString())
-      : undefined;
+	try {
+		const nodeId = Number.parseInt(req.params.nodeId);
+		const count = req.query.count
+			? Number.parseInt(req.query.count.toString())
+			: undefined;
+		const timeFrom = req.query.time_from
+			? Number.parseInt(req.query.time_from as string)
+			: undefined;
+		const timeTo = req.query.time_to
+			? Number.parseInt(req.query.time_to as string)
+			: undefined;
 
-    // find node
-    const node = await prisma.node.findFirst({
-      where: {
-        node_id: nodeId,
-      },
-    });
+		// find node
+		const node = await prisma.node.findFirst({
+			where: {
+				node_id: nodeId,
+			},
+		});
 
-    // make sure node exists
-    if (!node) {
-      res.status(404).json({
-        message: "Not Found",
-      });
-      return;
-    }
+		// make sure node exists
+		if (!node) {
+			res.status(404).json({
+				message: "Not Found",
+			});
+			return;
+		}
 
-    // get latest device metrics
-    const deviceMetrics = await prisma.deviceMetric.findMany({
-      where: {
-        node_id: node.node_id,
-      },
-      orderBy: {
-        id: "desc",
-      },
-      take: count,
-    });
+		// get latest device metrics
+		const deviceMetrics = await prisma.deviceMetric.findMany({
+			where: {
+				node_id: node.node_id,
+				created_at: {
+					gte: timeFrom ? new Date(timeFrom) : undefined,
+					lte: timeTo ? new Date(timeTo) : undefined,
+				},
+			},
+			orderBy: {
+				id: "desc",
+			},
+			take: count,
+		});
 
-    res.json({
-      device_metrics: deviceMetrics,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong, try again later.",
-    });
-  }
+		res.json({
+			device_metrics: deviceMetrics,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something went wrong, try again later.",
+		});
+	}
 });
 console.log(
-  "API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/device-metrics"
+	"API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/device-metrics"
 );
 
 express.get("/api/v1/nodes/:nodeId/environment-metrics", async (req, res) => {
-  try {
-    const nodeId = Number.parseInt(req.params.nodeId);
-    const count = req.query.count
-      ? Number.parseInt(req.query.count.toString())
-      : undefined;
+	try {
+		const nodeId = Number.parseInt(req.params.nodeId);
+		const count = req.query.count
+			? Number.parseInt(req.query.count.toString())
+			: undefined;
 
-    // find node
-    const node = await prisma.node.findFirst({
-      where: {
-        node_id: nodeId,
-      },
-    });
+		// find node
+		const node = await prisma.node.findFirst({
+			where: {
+				node_id: nodeId,
+			},
+		});
 
-    // make sure node exists
-    if (!node) {
-      res.status(404).json({
-        message: "Not Found",
-      });
-      return;
-    }
+		// make sure node exists
+		if (!node) {
+			res.status(404).json({
+				message: "Not Found",
+			});
+			return;
+		}
 
-    // get latest environment metrics
-    const environmentMetrics = await prisma.environmentMetric.findMany({
-      where: {
-        node_id: node.node_id,
-      },
-      orderBy: {
-        id: "desc",
-      },
-      take: count,
-    });
+		// get latest environment metrics
+		const environmentMetrics = await prisma.environmentMetric.findMany({
+			where: {
+				node_id: node.node_id,
+			},
+			orderBy: {
+				id: "desc",
+			},
+			take: count,
+		});
 
-    res.json({
-      environment_metrics: environmentMetrics,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong, try again later.",
-    });
-  }
+		res.json({
+			environment_metrics: environmentMetrics,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something went wrong, try again later.",
+		});
+	}
 });
 console.log(
-  "API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/environment-metrics"
+	"API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/environment-metrics"
 );
 
 express.get("/api/v1/nodes/:nodeId/power-metrics", async (req, res) => {
-  try {
-    const nodeId = Number.parseInt(req.params.nodeId);
-    const count = req.query.count
-      ? Number.parseInt(req.query.count.toString())
-      : undefined;
+	try {
+		const nodeId = Number.parseInt(req.params.nodeId);
+		const count = req.query.count
+			? Number.parseInt(req.query.count.toString())
+			: undefined;
 
-    // find node
-    const node = await prisma.node.findFirst({
-      where: {
-        node_id: nodeId,
-      },
-    });
+		// find node
+		const node = await prisma.node.findFirst({
+			where: {
+				node_id: nodeId,
+			},
+		});
 
-    // make sure node exists
-    if (!node) {
-      res.status(404).json({
-        message: "Not Found",
-      });
-      return;
-    }
+		// make sure node exists
+		if (!node) {
+			res.status(404).json({
+				message: "Not Found",
+			});
+			return;
+		}
 
-    // get latest power metrics
-    const powerMetrics = await prisma.powerMetric.findMany({
-      where: {
-        node_id: node.node_id,
-      },
-      orderBy: {
-        id: "desc",
-      },
-      take: count,
-    });
+		// get latest power metrics
+		const powerMetrics = await prisma.powerMetric.findMany({
+			where: {
+				node_id: node.node_id,
+			},
+			orderBy: {
+				id: "desc",
+			},
+			take: count,
+		});
 
-    res.json({
-      power_metrics: powerMetrics,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong, try again later.",
-    });
-  }
+		res.json({
+			power_metrics: powerMetrics,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something went wrong, try again later.",
+		});
+	}
 });
 console.log(
-  "API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/power-metrics"
+	"API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/power-metrics"
 );
 
 express.get("/api/v1/nodes/:nodeId/mqtt-metrics", async (req, res) => {
-  try {
-    const nodeId = Number.parseInt(req.params.nodeId);
+	try {
+		const nodeId = Number.parseInt(req.params.nodeId);
 
-    // find node
-    const node = await prisma.node.findFirst({
-      where: {
-        node_id: nodeId,
-      },
-    });
+		// find node
+		const node = await prisma.node.findFirst({
+			where: {
+				node_id: nodeId,
+			},
+		});
 
-    // make sure node exists
-    if (!node) {
-      res.status(404).json({
-        message: "Not Found",
-      });
-      return;
-    }
+		// make sure node exists
+		if (!node) {
+			res.status(404).json({
+				message: "Not Found",
+			});
+			return;
+		}
 
-    // get mqtt topics published to by this node
-    const queryResult =
-      await prisma.$queryRaw`select mqtt_topic, count(*) as packet_count, max(created_at) as last_packet_at from service_envelopes where gateway_id = ${nodeId} group by mqtt_topic order by packet_count desc;`;
+		// get mqtt topics published to by this node
+		const queryResult =
+			await prisma.$queryRaw`select mqtt_topic, count(*) as packet_count, max(created_at) as last_packet_at from service_envelopes where gateway_id = ${nodeId} group by mqtt_topic order by packet_count desc;`;
 
-    res.json({
-      mqtt_metrics: queryResult,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong, try again later.",
-    });
-  }
+		res.json({
+			mqtt_metrics: queryResult,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something went wrong, try again later.",
+		});
+	}
 });
 console.log(
-  "API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/mqtt-metrics"
+	"API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/mqtt-metrics"
 );
