d17770ccc0fb60421e1233066130135afd905c49
liamcottle <liam@liamcottle.com>
2024-09-07 23:36:09 +1200

    filter nodes by hardware model

--- api/src/routes/nodes.ts
+++ api/src/routes/nodes.ts
@@ -3,54 +3,63 @@ import express from "../express.js";
 import { formatNodeInfo } from "../tools/format.js";
 
 express.get("/api/v1/nodes", async (req, res) => {
-  try {
-    // get nodes from db
-    const nodes = await prisma.node.findMany();
+	try {
+		// get query params
+		const hardwareModel = req.query.hardware_model
+			? Number.parseInt(req.query.hardware_model as string)
+			: undefined;
 
-    const nodesWithInfo = [];
-    for (const node of nodes) {
-      nodesWithInfo.push(formatNodeInfo(node));
-    }
+		// get nodes from db
+		const nodes = await prisma.node.findMany({
+			where: {
+				hardware_model: hardwareModel,
+			},
+		});
 
-    res.json({
-      nodes: nodesWithInfo,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong, try again later.",
-    });
-  }
+		const nodesWithInfo = [];
+		for (const node of nodes) {
+			nodesWithInfo.push(formatNodeInfo(node));
+		}
+
+		res.json({
+			nodes: nodesWithInfo,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something went wrong, try again later.",
+		});
+	}
 });
 console.log("API:EXPRESS registered route GET:/api/v1/nodes");
 
 express.get("/api/v1/nodes/:nodeId", async (req, res) => {
-  try {
-    const nodeId = Number.parseInt(req.params.nodeId);
+	try {
+		const nodeId = Number.parseInt(req.params.nodeId);
 
-    // find node
-    const node = await prisma.node.findFirst({
-      where: {
-        node_id: nodeId,
-      },
-    });
+		// find node
+		const node = await prisma.node.findFirst({
+			where: {
+				node_id: nodeId,
+			},
+		});
 
-    // make sure node exists
-    if (!node) {
-      res.status(404).json({
-        message: "Not Found",
-      });
-      return;
-    }
+		// make sure node exists
+		if (!node) {
+			res.status(404).json({
+				message: "Not Found",
+			});
+			return;
+		}
 
-    res.json({
-      node: formatNodeInfo(node),
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong, try again later.",
-    });
-  }
+		res.json({
+			node: formatNodeInfo(node),
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something went wrong, try again later.",
+		});
+	}
 });
 console.log("API:EXPRESS registered route GET:/api/v1/nodes/:nodeId");
